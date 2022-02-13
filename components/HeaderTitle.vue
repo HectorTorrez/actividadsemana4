669a93d11c5be5e818ef313a211171e6c5299496
Héctor Buruca <torrezhectorb@gmail.com>
2022-02-12 19:42:16 -0600

    peso, moneda y kilometros

---
+++ components/HeaderTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <nav class="navbar navbar-light bg-light ">
+            <div class="container-fluid ">
+                <p class="navbar-brand ">
+                    {{title}}
+                </p>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HeaderTitle',
+    props:{
+        title: String
+    }    
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-title title="Conversion"/>
+   <card/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderTitle from "../components/HeaderTitle.vue"
+import Card from "../components/Cards.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderTitle,
+    Card
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
